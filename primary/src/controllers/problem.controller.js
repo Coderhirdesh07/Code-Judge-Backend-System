0d8f59ad5450ac500aef5d9fa13fe365d4d310d4
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2025-08-15 22:19:11 +0530

    added queue related logic

--- primary/src/controllers/problem.controller.js
+++ primary/src/controllers/problem.controller.js
@@ -1,14 +1,27 @@
+const {sendToQueue} = require("../utils/queue.utils.js");
+const Problem = require("../models/problem.model.js");
 
+async function handleGetProblemRoute(request,response){
+    const {title} = request.body;
+    if(!title) response.status(200)
+        .json({message:"Title not found or null"});
 
-
-function handleGetProblemRoute(){
-
+    const problem = await Problem.findOne({title:title});
+    if(!problem) response.status(200).json({message:"Problem not found"});
+    return response.status(400)
+    .json({message:"Problem found",problem:problem});
 }
-function handleRunProblemRoute (){}
+function handleRunProblemRoute(request,response){
+    
 
 
 
-function handleSubmitProblemRoute(){
-
+    response.status(400).json({message:"Problem is running"});
 }
+
+function handleSubmitProblemRoute(request,response){
+
+    response.status(400).json({message:"Problem is submitting"});
+}
+
 module.exports = {handleSubmitProblemRoute,handleGetProblemRoute,handleRunProblemRoute};
