7d3fb5d85f592e734553cb5195481984553d090b
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2025-08-23 11:37:44 +0530

    updated code logic for queue and folder for better structuring of code

--- worker/src/queue/index.queue.js
+++ worker/src/queue/index.queue.js
@@ -1,29 +1,45 @@
 const amqp = require('amqplib/callback_api');
+const runCode = require('../executor/runCode');
+require('dotenv').config();
 
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+const queue = process.env.QUEUE_NAME;
+amqp.connect(process.env.RABBITMQ_URL, function(error0, connection) {
     if (error0) {
+      console.error('Failed to connect to rabbit mq server')
       throw error0;
     }
     connection.createChannel(function(error1, channel) {
       if (error1) {
+        console.error('Cannot connect to Rabbit MQ server')
         throw error1;
       }
-      var queue = 'hello';
+      
   
       channel.assertQueue(queue, {
         durable: false
       });
-      const item  = consumeFromQueue(channel)
+      
+      console.log(` Worker listening on queue '${queue}'...`);
+
+      channel.consume(queue,
+        async function(msg){
+          if(!msg) return;
+          console.log('Listening from the queue');
+
+          try{
+            const Job = JSON.parse(msg.content.toString());
+            const {code,language} = Job;
+
+            const result = await runCode(code,language);
+            console.log('Execution result is in progress');
+
+          }
+          catch(error){
+            console.error('Error processing job',err);
+          }
+        }
+      )
     });
   });
 
-
-  function consumeFromQueue(channel){
-    channel.consume('hello',function(msg){
-        console.log('Recieved from queue');
-    },{
-        noAck:true
-    });
-    return msg;
-  }
